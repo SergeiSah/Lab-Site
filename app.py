92a52f2a6575f82c37b780afacbe2822a068fce9
SergeiSakh <sergei.sakhonenkov@gmail.com>
2024-01-31 00:48:54 +0300

    Added BINDS

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 from plugins import admin, db, login_manager
-from models import *
+from site_models import *
 from routes import main_bp
 
 
@@ -9,10 +9,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
     app.config['SECRET_KEY'] = Config.SECRET_KEY  # секретный ключ для хэширования данных сессии
+    app.config['SQLALCHEMY_BINDS'] = Config.SQLALCHEMY_BINDS
 
     app.register_blueprint(main_bp)
 
     db.init_app(app)
+
     admin.init_app(app)
     login_manager.init_app(app)
 
--- config.py
+++ config.py
@@ -5,3 +5,7 @@ class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "site.db")}'
+    SQLALCHEMY_BINDS = {
+        'Elements': f'sqlite:///{os.path.join(BASE_DIR, "x-ray-mirrors/data_collections/Elements.db")}',
+        'Compounds': f'sqlite:///{os.path.join(BASE_DIR, "x-ray-mirrors/data_collections/Compounds.db")}'
+    }
